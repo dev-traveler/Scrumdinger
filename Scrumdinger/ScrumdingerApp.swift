ef0e90831eac9b5b8c45986ab5115966a9d1f6a1
dev_traveler <avenj.dev@gmail.com>
2023-01-06 17:31:14 +0900

    first commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by JAEHYUN KIM on 2023/01/06.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
