c43d6d6645eed51b9a53ecb7673a712a0f6b91dd
dev_traveler <avenj.dev@gmail.com>
2023-01-09 15:27:48 +0900

    Done: Displaying data in list

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -31,9 +31,7 @@ enum Theme: String {
         case .indigo, .magenta, .navy, .oxblood, .purple: return .white
         }
     }
-    
     var mainColor: Color {
         Color(rawValue)
     }
 }
-
